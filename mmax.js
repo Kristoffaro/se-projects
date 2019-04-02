bf526e8f09cf292169f5780a5a91e3b4f528e61a
Nataliia Romanenko <924473@gmail.com>
2019-04-02 18:56:39 -0400

    added basic HTML

---
+++ mmax.js
@@ -0,0 +1,24 @@
+// default header for all pages except forms
+var header = '<nav class="navbar navbar-expand-sm bg-dark navbar-dark">' +
+    '<a class="navbar-brand" href="index.html"> <span class="logo">MovieMax</span></a>' +
+    '<form class="form-inline col-sm " action="#"> <input class=' +
+    '"form-control mr-sm-2  col-sm-8" type="text" placeholder="Search movie">' +
+    '<button class="btn btn-primary" type="submit">Search</button> </form>' +
+    '<ul class="navbar-nav"><li class="nav-item"><a class="nav-link" href="login.html">' +
+    'Log in</a> </li> <li class="nav-item"><a class="nav-link" href="#">Cart</a>' +
+    '</li></ul></nav>'
+
+// default footer for all pages
+var footer = '<div class="col col-sm-4" style="">' +
+    '<ul class="list-unstyled"><li class="nav-item"><a href="#">Your Account</a>' +
+    '</li><li class="nav-item"><a href="register.html">Register</a></li><p><small>' +
+    '&copy; 2019 MovieMax</small></p></ul></div><div class="col col-sm-4">' +
+    '<ul class="list-unstyled"><li class="nav-item"><a href="terms.html">Terms of Use</a>' +
+    '</li><li class="nav-item"><a href="privacy.html">Privacy Policy</a></li></ul></div>' +
+    '<div class="col col-sm-4"><ul class="list-unstyled"><li class="nav-item">' +
+    '<a href="contact.html">Contact Us</a></li><li class="nav-item"><a href="about.html">' +
+    'About us</a></li></ul></div>'
+
+// header for pages with forms
+var forms_header = '<nav class="navbar navbar-expand-sm bg-dark navbar-dark">' +
+    '<a class="navbar-brand" href="index.html"> <span class="logo">MovieMax</span></a></nav>'
