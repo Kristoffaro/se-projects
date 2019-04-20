7ea95e5fbffac30cd8d0236cf6b1a441517270c3
Tan Dung Luong <nyczpete@gmail.com>
2019-04-20 17:22:01 -0400

    added stuff

--- mmax.js
+++ mmax.js
@@ -28,11 +28,63 @@ var header_loggedIn = '<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
     '<form class="form-inline col-sm " action="search.html"> <input class=' +
     '"form-control mr-sm-2  col-sm-8" type="text" placeholder="Search movie">' +
     '<button class="btn btn-primary" type="submit">Search</button> </form>' +
-    '<ul class="navbar-nav"><li class="nav-item dropdown">'+
-    '<a class="nav-link dropdown-toggle" href="/se-projects/account/index.html" id="navbardrop" data-toggle="dropdown">'+
+    '<ul class="navbar-nav"><li class="nav-item dropdown">' +
+    '<a class="nav-link dropdown-toggle" href="/se-projects/account/index.html" id="navbardrop" data-toggle="dropdown">' +
     'My account</a><div class="dropdown-menu">' +
-    '<a class="dropdown-item" href="/se-projects/account/edit.html">Edit profile</a>'+
-    '<a class="dropdown-item" href="/se-projects/account/library.html">Movie library</a>'+
+    '<a class="dropdown-item" href="/se-projects/account/edit.html">Edit profile</a>' +
+    '<a class="dropdown-item" href="/se-projects/account/library.html">Movie library</a>' +
     '<a class="dropdown-item" href="/se-projects/account/history.html">History</a></div></li>' +
     '<li class="nav-item"><a class="nav-link" href="/se-projects/cart.html">Cart</a>' +
     '</li></ul></nav>'
+
+function url_query( query ) {
+    query = query.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+    var expr = "[\\?&]"+query+"=([^&#]*)";
+    var regex = new RegExp( expr );
+    var results = regex.exec( window.location.href );
+    if ( results !== null ) {
+        return results[1];
+    } else {
+        return false;
+    }
+}
+
+const movies_endpoint = 'https://j0l1npgx02.execute-api.us-east-2.amazonaws.com/prod-live/movies/';
+const ul = document.getElementById('movies');
+
+// get movie with match
+function getMovies(searchString, ul) {
+    window.fetch(movies_endpoint + toTitleCase(searchString))
+        .then((resp) => resp.json())
+        .then(function (data) {
+            let movies = data.results;
+            return movies.map(function (movie) {
+                // this is for testing purposes, will change to return list once i can get the endpoint working locally
+                let li = createNode('li'),
+                    span = createNode('span');
+                span.innerHTML = `${movie.title} ${movie.year}`;
+                append(li, span);
+                append(ul, li);
+            })
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+    return parent.appendChild(el);
+}
+
+function toTitleCase(str) {
+    return str.replace(
+        /\w\S*/g,
+        function (txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
+}
