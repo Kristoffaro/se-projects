0d735754510c685a8acb112e07f6ff11cbf77743
Tan Dung Luong <nyczpete@gmail.com>
2019-04-20 22:05:27 -0400

    Created new js file
    Working with ghibli data in the mean time

--- mmax.js
+++ mmax.js
@@ -37,80 +37,15 @@ var header_loggedIn = '<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
     '<li class="nav-item"><a class="nav-link" href="/se-projects/cart.html">Cart</a>' +
     '</li></ul></nav>'
 
-function url_query( query ) {
-    query = query.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
-    var expr = "[\\?&]"+query+"=([^&#]*)";
-    var regex = new RegExp( expr );
-    var results = regex.exec( window.location.href );
-    if ( results !== null ) {
+// pulls data from url
+function url_query(query) {
+    query = query.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var expr = "[\\?&]" + query + "=([^&#]*)";
+    var regex = new RegExp(expr);
+    var results = regex.exec(window.location.href);
+    if (results !== null) {
         return results[1];
     } else {
         return false;
     }
-}
-
-const movies_endpoint = 'https://j0l1npgx02.execute-api.us-east-2.amazonaws.com/prod-live/movies/';
-const ul = document.getElementById('moviesList');
-
-// get movie with match
-function getMovies(searchString) {
-    window.fetch(movies_endpoint + toTitleCase(searchString))
-        .then((resp) => resp.json())
-        .then(function (data) {
-            let movies = data.results;
-            return movies.map(function (movie) {
-                // this is for testing purposes, will change to return list once i can get the endpoint working locally
-                let li = createNode('li'),
-                    span = createNode('span');
-                span.innerHTML = `${movie.title} ${movie.year}`;
-                append(li, span);
-                append(ul, li);
-            })
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-}
-
-// get movie function 2
-function getMovies2(searchString){
-// Create a request variable and assign a new XMLHttpRequest object to it.
-var request = new XMLHttpRequest()
-
-// Open a new connection, using the GET request on the URL endpoint
-// ghibliapi is a placeholder until i get our movies endpoint working
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
-
-request.onload = function () {
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response)
-
-    if (request.status >= 200 && request.status < 400) {
-        data.forEach(movie => {
-        console.log(movie.title)
-    })
-    } else {
-        console.log('error')
-    }
-  }
-
-// Send request
-request.send()
-}
-
-function createNode(element) {
-    return document.createElement(element);
-}
-
-function append(parent, el) {
-    return parent.appendChild(el);
-}
-
-function toTitleCase(str) {
-    return str.replace(
-        /\w\S*/g,
-        function (txt) {
-            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-        }
-    );
 }
