717dc317e2f9191fad3049e6b13289612d258a5b
Tan Dung Luong <nyczpete@gmail.com>
2019-04-20 23:18:29 -0400

    yay works!

--- implementation.js
+++ implementation.js
@@ -20,9 +20,8 @@ function getMovies(searchString) {
     request.onload = function () {
         // Begin accessing JSON data here
         var data = JSON.parse(this.response);
-
         if (request.status >= 200 && request.status < 400) {
-            data.forEach(movie => {
+            data.movies.forEach(movie => {
                 const card = document.createElement('div')
                 card.setAttribute('class', 'row shadow p-3 mb-5 bg-white')
 
@@ -31,22 +30,32 @@ function getMovies(searchString) {
                 const link = document.createElement('a')
                 link.setAttribute('href', 'movie.html')
                 const img = document.createElement('IMG')
-                img.setAttribute('src', 'poster.jpg')
+                img.setAttribute('src', movie.poster)
                 img.setAttribute('class', 'img-fluid')
 
                 const infoCard = document.createElement('div')
                 infoCard.setAttribute('class', 'col-10')
 
                 const h3 = document.createElement('h3')
-                h3.textContent = movie.title
+                h3.textContent = movie.title + ' (' + movie.year + ')'
 
-                const p = document.createElement('p')
-                movie.description = movie.description.substring(0, 300)
-                p.textContent = `${movie.description}...`
+                const actors = document.createElement('i')
+                actors.textContent = 'Cast: ' + movie.actors
+                const br = document.createElement('br')
+
+                const director = document.createElement('i')
+                director.textContent = 'Director: ' + movie.director
+
+                const plot = document.createElement('p')
+                movie.plot = movie.plot.substring(0, 300)
+                plot.textContent = `${movie.plot}...`
                 
                 link.appendChild(img)
                 infoCard.appendChild(h3)
-                infoCard.appendChild(p)
+                infoCard.appendChild(actors)
+                infoCard.appendChild(br)
+                infoCard.appendChild(director)
+                infoCard.appendChild(plot)
                 imgCard.appendChild(link)
                 container.appendChild(card)
                 card.appendChild(imgCard)
