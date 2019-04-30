c4b0063aa40f73de7ac10491442dc4903cb98338
Tan Dung Luong <nyczpete@gmail.com>
2019-04-29 21:38:41 -0400

    Update libraryImpl.js

--- libraryImpl.js
+++ libraryImpl.js
@@ -1,58 +1,60 @@
 function getLibrary() {
-    const app = document.getElementById('library')
-    const movies_endpoint = 'https://j0l1npgx02.execute-api.us-east-2.amazonaws.com/prod-live/movies/'
-    var movieList = [];
+    if (movieOwned.length > 0 || movieRent.length > 0) {
+        const app = document.getElementById('library')
+        const movies_endpoint = 'https://j0l1npgx02.execute-api.us-east-2.amazonaws.com/prod-live/movies/'
+        var movieList = [];
 
-    var libArray = userOwned.split(',')
+        var libArray = userOwned.split(',')
 
-    const container = document.createElement('div')
-    container.setAttribute('class', 'container')
-    app.appendChild(container)
+        const container = document.createElement('div')
+        container.setAttribute('class', 'container')
+        app.appendChild(container)
 
-    const h1 = document.createElement('h1')
-    h1.textContent = 'Movies in Library'
-    container.appendChild(h1)
+        const h1 = document.createElement('h1')
+        h1.textContent = 'Movies in Library'
+        container.appendChild(h1)
 
-    const row1 = document.createElement('div')
-    row1.setAttribute('class', 'row')
+        const row1 = document.createElement('div')
+        row1.setAttribute('class', 'row')
 
-    container.appendChild(row1)
+        container.appendChild(row1)
 
 
-    var j = 0;
+        var j = 0;
 
-    libArray.forEach(element => {
-        var request = new XMLHttpRequest()
-        request.open('GET', movies_endpoint + element, true)
-        request.onload = function () {
-            var data = JSON.parse(this.response);
-            if (request.status >= 200 && request.status < 400) {
-                movieList.push(data.movies[0])
-                if (j < 4) {
-                    const col = document.createElement('div')
-                    col.setAttribute('class', 'col')
+        libArray.forEach(element => {
+            var request = new XMLHttpRequest()
+            request.open('GET', movies_endpoint + element, true)
+            request.onload = function () {
+                var data = JSON.parse(this.response);
+                if (request.status >= 200 && request.status < 400) {
+                    movieList.push(data.movies[0])
+                    if (j < 4) {
+                        const col = document.createElement('div')
+                        col.setAttribute('class', 'col')
 
-                    const link = document.createElement('a')
-                    link.setAttribute('href', 'movie.html')
+                        const link = document.createElement('a')
+                        link.setAttribute('href', 'movie.html')
 
-                    link.onclick = (function () {
-                        var currentI = i;
-                        return function () {
-                            saveMovie(currentI + '');
-                        }
-                    })();
+                        link.onclick = (function () {
+                            var currentI = i;
+                            return function () {
+                                saveMovie(currentI + '');
+                            }
+                        })();
 
-                    const img = document.createElement('IMG')
-                    img.setAttribute('src', data.movies[0].poster)
-                    img.setAttribute('class', 'img-fluid')
+                        const img = document.createElement('IMG')
+                        img.setAttribute('src', data.movies[0].poster)
+                        img.setAttribute('class', 'img-fluid')
 
-                    link.appendChild(img)
-                    col.appendChild(link)
-                    row1.appendChild(col)
+                        link.appendChild(img)
+                        col.appendChild(link)
+                        row1.appendChild(col)
+                    }
                 }
             }
-        }
-        request.send()
-        j++
-    }); (userOwned.split(','))
+            request.send()
+            j++
+        }); (userOwned.split(','))
+    }
 }
