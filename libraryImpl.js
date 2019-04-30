9e83bfc5f482aafe42308e81730c2e156a656eb5
Tan Dung Luong <nyczpete@gmail.com>
2019-04-29 22:04:46 -0400

    Update libraryImpl.js

--- libraryImpl.js
+++ libraryImpl.js
@@ -1,38 +1,41 @@
+const movies_endpoint = 'https://j0l1npgx02.execute-api.us-east-2.amazonaws.com/prod-live/all/';
+const app = document.getElementById('library');
+var movieList = [];
+const library = getOwned.split(', ')
+
 function getLibrary() {
     if (userOwned.length > 0 || userRent.length > 0) {
-        const app = document.getElementById('library')
-        const movies_endpoint = 'https://j0l1npgx02.execute-api.us-east-2.amazonaws.com/prod-live/movies/'
-        var movieList = [];
-
-        var libArray = userOwned.split(',')
-
         const container = document.createElement('div')
         container.setAttribute('class', 'container')
         app.appendChild(container)
 
         const h1 = document.createElement('h1')
-        h1.textContent = 'Movies in Library'
+        h1.textContent = 'Library'
         container.appendChild(h1)
 
         const row1 = document.createElement('div')
         row1.setAttribute('class', 'row')
-
+        const row2 = document.createElement('div')
+        row2.setAttribute('class', 'row')
         container.appendChild(row1)
+        container.appendChild(row2)
 
-        for (var i = 0; i < libArray.length; i++) {
-            var request = new XMLHttpRequest()
-            request.open('GET', movies_endpoint + libArray[i].trim(), true)
-            request.onload = function () {
-                var data = JSON.parse(this.response);
-                if (request.status >= 200 && request.status < 400) {
-                    movieList.push(data.movies[0])
-                    if (i < 4) {
+        var request = new XMLHttpRequest()
+        request.open('GET', movies_endpoint + 'a', true)
+
+        request.onload = function () {
+            var data = JSON.parse(this.response);
+            if (request.status >= 200 && request.status < 400) {
+                var j = 0;
+                for (var i = 0; i < data.movies.length; i++) {
+
+                    movieList.push(data.movies[i])
+                    if (j < 4 && library.includes(data.movies[i].title)) {
                         const col = document.createElement('div')
                         col.setAttribute('class', 'col')
 
                         const link = document.createElement('a')
                         link.setAttribute('href', 'movie.html')
-
                         link.onclick = (function () {
                             var currentI = i;
                             return function () {
@@ -41,16 +44,40 @@ function getLibrary() {
                         })();
 
                         const img = document.createElement('IMG')
-                        img.setAttribute('src', data.movies[0].poster)
+                        img.setAttribute('src', data.movies[i].poster)
                         img.setAttribute('class', 'img-fluid')
 
                         link.appendChild(img)
                         col.appendChild(link)
                         row1.appendChild(col)
+                        j++
+                    } else if (j < 8 && library.includes(data.movies[i].title)) {
+                        const col = document.createElement('div')
+                        col.setAttribute('class', 'col')
+
+                        const link = document.createElement('a')
+                        link.setAttribute('href', 'movie.html')
+                        link.onclick = (function () {
+                            var currentI = i;
+                            return function () {
+                                saveMovie(currentI + '');
+                            }
+                        })();
+
+                        const img = document.createElement('IMG')
+                        img.setAttribute('src', data.movies[i].poster)
+                        img.setAttribute('class', 'img-fluid')
+
+                        link.appendChild(img)
+                        col.appendChild(link)
+                        row2.appendChild(col)
+                        j++
                     }
                 }
+            } else {
+                console.log("Error")
             }
-            request.send()
         }
+        request.send();
     }
 }
