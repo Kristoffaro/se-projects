fc491730385f2e512ba06d29007b328720283949
Tan Dung Luong <nyczpete@gmail.com>
2019-05-01 00:02:41 -0400

    Update streamImpl.js

--- account/streamImpl.js
+++ account/streamImpl.js
@@ -2,16 +2,17 @@ function init() {
     gapi.client.setApiKey("AIzaSyBmyogOtizklthGZI6j9_XFRgX7tvaNxFI");
     gapi.client.load("youtube", "v3", function () {
         // prepare request
-    var request = gapi.client.youtube.search.list({
-        part: "snippet",
-        type: "video",
-        q: encodeURIComponent('Avengers+2012+trailer'),
-        maxResults: 1
-    });
+        var request = gapi.client.youtube.search.list({
+            part: "snippet",
+            type: "video",
+            q: encodeURIComponent(movieTitle + '+' + movieYear + '+trailer'),
+            maxResults: 1
+        });
 
-    // execute request
-    request.execute(function (response) {
-        console.log(response);
-    });
+        // execute request
+        request.execute(function (response) {
+            var results = response.result;
+            console.log(results.items.item);
+        });
     });
 }
