8c5458c86832c3e540fd0d4b58c138e0f8bcf7b0
Tan Dung Luong <nyczpete@gmail.com>
2019-05-01 00:18:43 -0400

    test more

--- account/streamImpl.js
+++ account/streamImpl.js
@@ -2,18 +2,18 @@ function init() {
     gapi.client.setApiKey("AIzaSyBmyogOtizklthGZI6j9_XFRgX7tvaNxFI");
     gapi.client.load("youtube", "v3", function () {
         // prepare request
+        var search = movieTitle.concat(' ' + movieYear + ' trailer').replace(/%20/g, '+')
         var request = gapi.client.youtube.search.list({
             part: "snippet",
             type: "video",
-            q: encodeURIComponent(movieTitle),
-            //q: encodeURIComponent(movieTitle.concat('+' + movieYear + '+trailer')),
+            q: encodeURIComponent(search),
             maxResults: 1
         });
 
         // execute request
         request.execute(function (response) {
             var results = response.result;
-            console.log(results.items[0]);
+            $("results").append(results.items[0].id.videoId + ' ' + results.items[0].snippet.title);
         });
     });
 }
