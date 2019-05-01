e027c22cb19278e03890b3e662c2dd519f2d7ac6
Tan Dung Luong <nyczpete@gmail.com>
2019-05-01 01:08:57 -0400

    stream impl

--- account/streamImpl.js
+++ account/streamImpl.js
@@ -1,4 +1,6 @@
-const resultsApp = document.getElementById('results');
+function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){return t[n][r]})}return res}
+const resultsApp = document.getElementById('videoPlayer');
+var searchString = movieTitle.concat(' trailer')
 
 function init() {
     gapi.client.setApiKey("AIzaSyBmyogOtizklthGZI6j9_XFRgX7tvaNxFI");
@@ -16,7 +18,7 @@ function init() {
             var results = response.result;
             console.log(movieTitle);
             console.log(response);
-            resultsApp.innerHTML = results.items[0].id.videoId + ' ' + results.items[0].snippet.title;
+            resultsApp.src="https://www.youtube.com/embed/" + results.items[0].id.videoId;
         });
     });
 }
