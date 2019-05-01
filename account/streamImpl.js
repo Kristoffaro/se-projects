b317c5aa5a37741ae2d547793d603d3ae09b102b
Tan Dung Luong <nyczpete@gmail.com>
2019-05-01 00:29:48 -0400

    Update streamImpl.js

--- account/streamImpl.js
+++ account/streamImpl.js
@@ -14,7 +14,7 @@ function init() {
         request.execute(function (response) {
             var results = response.result;
             console.log(search.replace(/\s/g, "+"));
-            console.log(results.items[0]);
+            console.log(results.items);
             //$("results").append(results.items[0].id.videoId + ' ' + results.items[0].snippet.title);
         });
     });
