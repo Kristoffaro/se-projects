16a660ed231d86848f2d599e178856cd75c35e06
Tan Dung Luong <nyczpete@gmail.com>
2019-05-01 00:39:36 -0400

    Update streamImpl.js

--- account/streamImpl.js
+++ account/streamImpl.js
@@ -2,17 +2,16 @@ function init() {
     gapi.client.setApiKey("AIzaSyBmyogOtizklthGZI6j9_XFRgX7tvaNxFI");
     gapi.client.load("youtube", "v3", function () {
         // prepare request
-        var search = movieTitle.concat(' trailer')
         var request = gapi.client.youtube.search.list({
             part: "snippet",
             type: "video",
-            q: encodeURIComponent(search.replace(/\s/g, "+")),
+            q: encodeURIComponent(movieTitle),
             maxResults: 1
         });
 
         // execute request
         request.execute(function (response) {
-            console.log(search.replace(/\s/g, "+"));
+            console.log(movieTitle);
             console.log(response);
             //$("results").append(results.items[0].id.videoId + ' ' + results.items[0].snippet.title);
         });
